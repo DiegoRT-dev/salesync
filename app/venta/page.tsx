a24335f588c56babd0e7902e7aceb28d1ce88e50
DiegoRT-dev <diegordriguez02@gmail.com>
2026-04-15 13:31:05 -0600

    Correccion en venta

--- app/venta/page.tsx
+++ app/venta/page.tsx
@@ -1,12 +1,10 @@
 import Venta from "@/components/Venta";
-import { prisma } from "@/lib/prisma";
 
 export default async function VentaPage(){
-    const productos = await prisma.producto.findMany();
 
     return(
         <div>
-            <Venta productos={productos}/>
+            <Venta />
         </div>
     );
 }
